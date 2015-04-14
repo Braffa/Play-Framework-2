0c9dccfdcc6aef675b6e0865250855145d4179c4
Braffa <dave.brayfield@gmail.com>
2015-04-14 13:30:08 +0100

    DAB-006 added chapter04d

---
+++ app/models/chapter04d/Address.scala
@@ -0,0 +1,10 @@
+package models.chapter04d
+
+/**
+ * Created by david.a.brayfield on 14/04/2015.
+ */
+case class Address  (fullStreet: String,
+                     county: String,
+                     country: String) {
+
+}
